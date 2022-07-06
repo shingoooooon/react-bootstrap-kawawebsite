ed42deb83f7c3824ed389e024c81961e8fdb5d82
shingo kubomura <skksb75@gmail.com>
2022-07-06 16:35:15 +0900

    set routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
+import Gallery from './components/Gallery';
 import Header from './components/Header';
+import Home from './components/Home';
+import Works from './components/Works';
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom"
 
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/works" element={<Works />} />
+          <Route path="/gallery" element={<Gallery />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,27 @@
 
 import React from 'react'
+import {Link} from "react-router-dom"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/works">Works</Link>
+        </li>
+        <li>
+          <Link to="/gallery">Gallery</Link>
+        </li>
+        <li>
+          <Link to="#">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+
+
   )
 }
 
